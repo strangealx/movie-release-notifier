25a4828b02982c67b3b559fef698d73497b3c565
strangealx <strangealx@gmail.com>
2018-11-18 22:59:34 +0700

    update structure
    - move logic from root to ./app

---
+++ app/index.js
@@ -0,0 +1,33 @@
+const Parser = require('./Parser');
+const DBRelease = require('./db/models/MongoReleaseModel');
+const ParserList = require('./ParserList');
+const KinopoiskDigitalRelease = require('./Release/KinopoiskDigitalRelease');
+const MetacriticRelease = require('./Release/MetacriticRelease');
+const { kinopoiskConfig, metacriticConfig } = require('../config/parser/config');
+require('./db/connection');
+
+const parserList = new ParserList({ interval: 60 * 1e3 });
+
+DBRelease.toBeReleased()
+    .then((saved) => {
+        console.log(saved);
+        parserList
+            .addNewParser(new Parser(KinopoiskDigitalRelease, kinopoiskConfig))
+            .addNewParser(new Parser(MetacriticRelease, metacriticConfig))
+            .on('parser:data', (releaseList) => {
+                let count = 0;
+                releaseList.forEach((release) => {
+                    const dbRelease = new DBRelease(release);
+                    dbRelease.saveOrUpdate()
+                        .then((doc) => {
+                            const { data, type } = doc;
+                            count += 1;
+                            console.log(`${count}. ${type}: ${data.name.ru || data.name.en}`);
+                        })
+                        .catch(console.error.bind(console, 'saveOrUpdate error: '));
+                });
+            })
+            .on('parser:error', console.error.bind(console, 'parser error: '))
+            .run();
+    })
+    .catch(console.error.bind(console, 'db error: '));
