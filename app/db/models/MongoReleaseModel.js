26dd9a21aedbabef504bab005df021001afab2b6
strangealx <strangealx@gmail.com>
2019-08-11 15:07:31 +0700

    schemas to schema

--- app/db/models/MongoReleaseModel.js
+++ app/db/models/MongoReleaseModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoReleaseSchema = require('../schemas/mongoReleaseSchema');
+const mongoReleaseSchema = require('../schema/mongoReleaseSchema');
 
 const MongoRelease = mongoose.model('MongoRelease', mongoReleaseSchema);
 
--- app/db/models/tests/MongoReleaseModel.spec.js
+++ app/db/models/tests/MongoReleaseModel.spec.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoReleaseSchema = require('../../schemas/mongoReleaseSchema');
+const mongoReleaseSchema = require('../../schema/mongoReleaseSchema');
 const {
     username,
     password,
