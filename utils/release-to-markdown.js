11f1b3b2e372ddf7ceb499f993540f05e8a3b06d
strangealx <strangealx@gmail.com>
2019-08-13 23:22:36 +0700

    fix message name
    - fix en name

--- utils/release-to-markdown.js
+++ utils/release-to-markdown.js
@@ -9,7 +9,7 @@ const releaseToMarkdown = (release) => {
         metacritic_score: mcScore,
         metacritic_user_score: mcuScore,
     } = rating || {};
-    output += `Теперь "*${nameRu || nameEn}" ${!nameRu && nameEn ? `(${nameEn})` : ''}* доступен в качестве.\r\n`;
+    output += `Теперь "*${nameRu || nameEn}" ${nameRu && nameEn ? `(${nameEn})` : ''}* доступен в качестве.\r\n`;
     if (kpScore) output += `_Кинопоиск: ${kpScore}_\r\n`;
     if (mcScore) output += `_Метакритик: ${mcScore}${mcuScore ? ` | ${mcuScore}` : ''}_`;
     return output;
